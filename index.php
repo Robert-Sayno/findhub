882baa4bbae0743880c722d5da12b42dfcd3816f
avanteoninnovation <avanteontechinnovations@gmail.com>
2024-01-04 23:57:33 +0300

    done with auth and landing page

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Custom Website</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="wrapper">
+			<nav class="navbar">
+				<img class="logo" src="findhublogo.png">
+				<ul>
+					<li><a class="active" href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#">Feedback</a></li>
+				</ul>
+			</nav>
+			<div class="center">
+			<h1>At findhubuganda,</h1>
+			<h2>Let's find the lost</h2>
+			<div class="buttons">
+			<button>login</button>
+			<button class="btn2">Signup</button>
+		</div>
+		</div>
+</body>
+</html>
